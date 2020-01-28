323273e32c4af979b2e8b8345fac930ef25e7521
Alison <alisonipuc@gmail.com>
2020-01-28 11:32:58 -0300

    First Commit

---
+++ OpenSSLCryptography/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OpenSSLCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crypt = new OpenSSL();
+            var text = "BMW:SITE";
+            var encrypt = crypt.AESEncrypt(text);
+
+            Console.WriteLine("Welcome to OpenSSL Cryptography and SSL / TLS Toolkit");
+            Console.WriteLine();
+            Console.WriteLine("Cryptography " + text);
+            Console.WriteLine("Encrypt " + encrypt);
+            Console.WriteLine("Descrypt " + crypt.AESDecrypt(encrypt));
+            
+            encrypt = crypt.OpenSSLEncrypt(text, "secrect word pass !@#$%");
+            Console.WriteLine();
+            Console.WriteLine("Cryptography " + text);
+            Console.WriteLine("Encrypt " + encrypt);
+            Console.WriteLine("Descrypt " + crypt.OpenSSLDecrypt(encrypt, "secrect word pass !@#$%"));            
+        }
+    }
+}
